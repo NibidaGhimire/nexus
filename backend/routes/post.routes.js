4d082f6d314f4097eb26d7e354d15dc430b24c87
NibidaGhimire <nibidaghimire39@gmail.com>
2025-03-04 23:06:24 +0545

    fix route

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -3,7 +3,6 @@ import {
   addComment,
   addReview,
   createPost,
-  getComments,
   getPosts,
   upload,
 } from "../controllers/post.controller.js";
@@ -15,6 +14,5 @@ router.post("/create", protectRoute, upload.single("pdfFile"), createPost);
 router.get("/", getPosts);
 router.post("/:id/reviews",protectRoute, addReview);
 router.post("/:id/addComment", protectRoute, addComment);
-router.get("/:id/comments", protectRoute, getComments);
 
 export default router;
